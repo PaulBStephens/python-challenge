c11bf8acdae2e24f611071a38725565e2005b393
Paul Stephens <stepco99@yahoo.com>
2019-06-13 23:42:03 -0500

    adding a notebook file

---
+++ .ipynb_checkpoints/README-checkpoint.md
@@ -0,0 +1,2 @@
+# python-challenge
+Python homework due Wednesday, 10June2019
--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,17 @@
 
+import csv 
+
+
+
+with open("BeatlesSimpleCSVfile.csv") as csvfile:
+    read_csv = csv.reader(csvfile, delimiter=",")
+    if csv.Sniffer().has_header:
+        next(read_csv)
+    for row in read_csv:
+        count_members += 1
+        
+count_members
+print(f"The number of band members is {count_members}")
+
+
+
