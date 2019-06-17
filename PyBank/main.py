91c6364b071b635801238f69dfc64a4a65f6481b
Paul Stephens <stepco99@yahoo.com>
2019-06-16 23:23:40 -0500

    I'm commiting both PyBank and PyPoll

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,10 +12,8 @@ with open(file_to_load) as revenue_data:
     next(reader) 
     date = []
     revenue = []
-    rev_change = []    
     
-    # in this loop I did sum of column 2 (index 1) which is revenue in csv file and counted total months which is column 1 (index 0) 
-    # also needed to change the revenue data from string to float
+    # defined revenue as a float of row 1, and date as row 0 
     for row in reader:
         revenue.append(float(row[1]))
         date.append(row[0])
@@ -25,7 +23,7 @@ with open(file_to_load) as revenue_data:
     print("-----------------------------------")
     print("Total Months:", len(date))
     sum_of_revenue = '{0:.0f}'.format(sum(revenue))
-    print(f"Total Revenue: {sum_of_revenue}")
+    print(f"Total Revenue: ${sum_of_revenue}")
 
     #in this loop I did total of difference between all row of column "Revenue" and found total revnue change. Also found out max revenue change and min revenue change. 
     for i in range(1,len(revenue)):
@@ -33,25 +31,21 @@ with open(file_to_load) as revenue_data:
         avg_rev_change = sum(rev_change)/len(rev_change)
 
         max_rev_change = max(rev_change)
-
         min_rev_change = min(rev_change)
 
         max_rev_change_date = str(date[rev_change.index(max(rev_change))])
         min_rev_change_date = str(date[rev_change.index(min(rev_change))])
 
-    
-    print("Avereage Revenue Change: ", round(avg_rev_change))
-    print("Greatest Increase in Revenue to the Next Month:", max_rev_change_date,"($", max_rev_change,")")
-    print("Greatest Decrease in Revenue to the Next Month:", min_rev_change_date,"($", min_rev_change,")")
-
-    
-#   now need to export the same info I printed to a text file
-with open("output23.txt", "a") as f:
+    print("Average Revenue Change: $", '{0:.2f}'.format(avg_rev_change))
+    print("Greatest Increase in Revenue to the Next Month:", max_rev_change_date,"($", round(max_rev_change),")")
+    print("Greatest Decrease in Revenue to the Next Month:", min_rev_change_date,"($", round(min_rev_change),")")
+          
+## now need to export the same info I printed to a text file
+with open("outputPyBank.txt", "a") as f:
     print("Financial Analysis", file=f)
     print("-----------------------------------", file=f)
     print("Total Months:", len(date), file=f)
-    print("Total Revenue: $", sum(revenue), file=f)    
-    print("Avereage Revenue Change: ", round(avg_rev_change), file=f)
-    print("Greatest Increase in Revenue to the Next Month:", max_rev_change_date,"($", max_rev_change,")", file=f)
-    print("Greatest Decrease in Revenue to the Next Month:", min_rev_change_date,"($", min_rev_change,")", file=f)
-    
+    print(f"Total Revenue: ${sum_of_revenue}", file=f)
+    print("Average Revenue Change: $", '{0:.2f}'.format(avg_rev_change), file=f)
+    print("Greatest Increase in Revenue to the Next Month:", max_rev_change_date,"($", round(max_rev_change),")", file=f)
+    print("Greatest Decrease in Revenue to the Next Month:", min_rev_change_date,"($", round(min_rev_change),")", file=f)
